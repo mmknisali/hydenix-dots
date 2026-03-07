72ff11382f5721fd3a2a2e81b7c8fcf6732a837b
false <false>
2026-03-07 06:46:33 -0800

    a7a2

--- modules/hm/lazy.nix
+++ modules/hm/lazy.nix
@@ -1,6 +1,6 @@
-{ ... }
+{ inputs, ... }:   
 
 {
-  imports = [ lazyvim.homeManagerModules.default ];
+  imports = [ inputs.lazyvim.homeManagerModules.default ];
   programs.lazyvim.enable = true;
 }
